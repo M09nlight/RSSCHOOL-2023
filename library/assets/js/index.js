89cecb2c4ffe92d02bca51ded1830f8900024554
ivanp <ivan.portyanko@yandex.ru>
2023-09-10 21:58:42 +0300

    feat: add modal register window

--- library/assets/js/index.js
+++ library/assets/js/index.js
@@ -1,5 +1,21 @@
 let profile = document.querySelector(".profile__img");
 let noAuthComponent = document.querySelector(".drop-menu-nav--no-auth");
+let loginBtn = document.querySelector("#login");
+let registerBtns = document.querySelectorAll(".register-target");
+let registerMenu = document.querySelector(".register");
+let registerCloseBtn = document.querySelector(".register__close-btn");
+
+let profileImg = document.querySelector(".profile__img");
+let profileUser = document.querySelector(".profile-user");
+
+let digitReaderName = document.querySelector("#reader-name");
+let digitCardNumber = document.querySelector("#reader-card-number");
+
+const regForm = document.getElementById("register-form");
+
+let localStorageUsers = [];
+let currentUser = {};
+let isLogged = false;
 
 profile.addEventListener("click", (e) => {
   noAuthComponent.classList.toggle("drop-menu-nav--active-target");
@@ -11,10 +27,100 @@ profile.addEventListener("click", (e) => {
   e.stopPropagation();
 });
 
-document.body.addEventListener("click", (event) => {
-  if (!event.target.closest(".drop-menu-nav--active-target")) {
+document.body.addEventListener("click", (e) => {
+  if (!e.target.closest(".drop-menu-nav--active-target")) {
     if (noAuthComponent.classList.contains("drop-menu-nav--active-target")) {
       noAuthComponent.classList.toggle("drop-menu-nav--active-target");
     }
   }
+  if (!e.target.closest(".register__inner")) {
+    if (registerMenu.classList.contains("register--active-target")) {
+      registerMenu.classList.toggle("register--active-target");
+    }
+  }
+  e.stopPropagation();
 });
+
+registerBtns.forEach((regBtn) => {
+  regBtn.addEventListener("click", (e) => {
+    registerMenu.classList.toggle("register--active-target");
+    noAuthComponent.classList.toggle("drop-menu-nav--active-target");
+    e.stopPropagation();
+  });
+});
+
+registerCloseBtn.addEventListener("click", (event) => {
+  registerMenu.classList.toggle("register--active-target");
+});
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+regForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(regForm);
+  let cardNumber = getRandomInt(10000000000, 99999999999)
+    .toString(16)
+    .toUpperCase();
+  currentUser = {
+    firstname: formData.get("firstname"),
+    lastname: formData.get("lastname"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    cardNumber,
+    visits: 1,
+    hasLibraryCard: false,
+    books: [],
+  };
+  profileImg.classList.toggle("profile__img--logged");
+
+  profileUser.classList.toggle("profile-user--not-logged");
+  profileUser.innerHTML = currentUser.firstname[0] + currentUser.lastname[0];
+  registerMenu.classList.toggle("register--active-target");
+
+  updateDigitalCards(currentUser);
+  getUsersData();
+  updateUsersData(currentUser);
+  isLogged = true;
+});
+
+function updateDigitalCards(currentUser) {
+  digitReaderName.value = currentUser.firstname + " " + currentUser.lastname;
+  digitReaderName.setAttribute("disabled", "");
+  digitCardNumber.value = currentUser.cardNumber;
+  digitCardNumber.setAttribute("disabled", "");
+
+  document.querySelector(".libcard_table__name").textContent =
+    "Your Library card";
+  document.querySelector(".libcard_table_btm").style.display = "none";
+  document.querySelector("#visits").textContent = currentUser.visits;
+  document.querySelector("#bonuses").textContent = 1212;
+  document.querySelector("#books").textContent = 0;
+  document
+    .querySelector(".libcard-items")
+    .classList.toggle("libcard-items--not-logged");
+  document
+    .querySelector("#libcard-notlogin")
+    .classList.toggle("librarycard_request--not-logged");
+  document
+    .querySelector("#libcard-login")
+    .classList.toggle("librarycard_request--logged");
+}
+
+function getUsersData() {
+  try {
+    let currentStorage = localStorage.getItem("users");
+
+    if (!!currentStorage) localStorageUsers = JSON.parse(currentStorage);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function updateUsersData(obj) {
+  localStorageUsers.push(obj);
+  localStorageUsers = [...new Set(localStorageUsers)];
+  localStorage.setItem("users", JSON.stringify(localStorageUsers));
+}
+if(isLogged ==)
