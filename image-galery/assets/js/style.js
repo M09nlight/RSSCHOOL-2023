ba6d443f4ad61e59d251e5c8b06895a7f2908699
ivanp <ivan.portyanko@yandex.ru>
2023-10-02 14:37:44 +0300

    feat: change style for img, and default images

--- image-galery/assets/js/style.js
+++ image-galery/assets/js/style.js
@@ -1,4 +1,4 @@
-let searchVal = "winter";
+let searchVal = "full moon";
 let limitVal = "9";
 let url = `https://api.unsplash.com/search/photos?query=${searchVal}&&per_page=${limitVal}&client_id=5968czt58_7r9BYrqK4NbSeQd5NYYuIAdO_NKP0Wkwo`;
 
